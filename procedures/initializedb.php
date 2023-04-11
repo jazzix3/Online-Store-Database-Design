8cc2c0c2cdc7ddf40f8a40cd71f7fcfbd2f42bc5
Jasmine Maquindang <jazzix3@gmail.com>
2023-04-11 15:08:08 -0700

    added itemCategory table & categories

--- procedures/initializedb.php
+++ procedures/initializedb.php
@@ -4,22 +4,13 @@ require ("dbconnect.php");
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
-$sql = "DROP TABLE IF EXISTS review;";
-if ($conn->query($sql) === false) {
-    exit("Error creating review table: " . $conn->error);
+$tables = array("review", "item", "itemCategory", "user");
+foreach ($tables as $table) {
+    $sql = "DROP TABLE IF EXISTS $table;";
+    if ($conn->query($sql) === false) {
+        exit("Error dropping $table table: " . $conn->error);
+    }
 }
-
-$sql = "DROP TABLE IF EXISTS item;";
-if ($conn->query($sql) === false) {
-    exit("Error creating item table: " . $conn->error);
-}
-
-$sql = "DROP TABLE IF EXISTS user;";
-if ($conn->query($sql) === false) {
-    exit("Error creating user table: " . $conn->error);
-}
-
 
 
 $userTable = "CREATE TABLE IF NOT EXISTS user (
@@ -33,17 +24,26 @@ if ($conn->query($userTable) === false) {
     exit("Error creating user table: " . $conn->error);
 }
 
+$itemCategoryTable = "CREATE TABLE itemCategory (
+    category VARCHAR(64) NOT NULL PRIMARY KEY);";
+    
+
+if ($conn->query($itemCategoryTable) === false) {
+    exit("Error creating itemCategory table: " . $conn->error);
+}
+
 
 
 $itemTable = "CREATE TABLE IF NOT EXISTS item (
     itemId INT(10) AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(64) NOT NULL,
     description TEXT(255),
-    category VARCHAR(64),
+    category VARCHAR(64) NOT NULL,
+    FOREIGN KEY (category) REFERENCES itemCategory(category),
     price DECIMAL(10,2),
     postDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    username VARCHAR(32) NOT NULL,
-    FOREIGN KEY (username) REFERENCES user(username));";
+    postedBy VARCHAR(32) NOT NULL,
+    FOREIGN KEY (postedBy) REFERENCES user(username));";
 
 if ($conn->query($itemTable) === false) {
     exit("Error creating item table: " . $conn->error);
@@ -56,9 +56,9 @@ $reviewTable = "CREATE TABLE review (
     remark TEXT (255),
     score VARCHAR(10),
     reviewDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    writtenBy VARCHAR(255),
-    forItem INT,
+    writtenBy VARCHAR(32) NOT NULL,
     FOREIGN KEY (writtenBy) REFERENCES user(username),
+    forItem INT(10) NOT NULL,
     FOREIGN KEY (forItem) REFERENCES item(itemId));";
 
 if ($conn->query($reviewTable) === false) {
@@ -69,30 +69,42 @@ if ($conn->query($reviewTable) === false) {
 
 $queries = array(
     "INSERT INTO user(username, password, firstName, lastName, email) VALUES
-    ('john1', 'p@ssword1', 'John', 'Smith', 'john.smith@example.com'),
-    ('jane2', 'p@ssword2', 'Jane', 'Doe', 'jane.doe@example.com'),
-    ('matt3', 'p@ssword3', 'Matt', 'Garcia', 'jason.garcia@example.com'),
-    ('lisa4', 'p@ssword4', 'Lisa', 'Kim', 'lisa.kim@example.com'),
-    ('alice5', 'p@ssword5', 'Alice', 'Williams', 'alice.williams@example.com')",
+        ('john1', 'p@ssword1', 'John', 'Smith', 'john.smith@example.com'),
+        ('jane2', 'p@ssword2', 'Jane', 'Doe', 'jane.doe@example.com'),
+        ('matt3', 'p@ssword3', 'Matt', 'Garcia', 'jason.garcia@example.com'),
+        ('lisa4', 'p@ssword4', 'Lisa', 'Kim', 'lisa.kim@example.com'),
+        ('alice5', 'p@ssword5', 'Alice', 'Williams', 'alice.williams@example.com')",
+
+    "INSERT INTO itemCategory(category) VALUES
+        ('Art & Collectibles'),
+        ('Baby & Kids'),
+        ('Clothing & Accessories'),
+        ('Electronics'),
+        ('Furniture'),
+        ('Home & Garden'),
+        ('Pet Supplies'),
+        ('Sporting Goods'),
+        ('Toys'),
+        ('Other')",
     
-    "INSERT INTO item(title, description, category, price, postDate, username) VALUES
-    ('Smartphone 14 Pro', 'Newest phone with improved camera and battery life', 'Electronics', '800.00', '2023-03-03 10:00:00', 'john1'),
-    ('Wireless headphones', 'World-class combination of noise cancelling performance and premium comfort', 'Electronics', '275.00', '2023-03-30 12:00:00', 'jane2'),
-    ('Artisan Stand Mixer', 'Mix ingredients with ease using this powerful and stylish stand mixer', 'Home', '250.00', '2023-03-17 13:15:00', 'jane2'),
-    ('Mountain Bike', 'Full suspension mountain bike with 29-inch wheels', 'Sports', '1000.00', '2023-03-28 16:00:00', 'matt3'),
-    ('Vintage Watch', 'Mechanical wristwatch from the 1950s', 'Fashion', '300.00', '2023-03-27 14:20:00', 'matt3'),
-    ('Leather Shoulder Bag', 'Womens crossbody bag with adjustable strap', 'Fashion', '400.00', '2023-03-13 10:30:00', 'lisa4'),
-    ('Coffee Maker', 'Programmable coffee machine with thermal carafe', 'Home', '50.00', '2023-03-07 08:15:00', 'lisa4')",
+    "INSERT INTO item(title, description, category, price, postDate, postedBy) VALUES
+        ('Smartphone 14 Pro', 'Newest phone with improved camera and battery life', 'Electronics', '800.00', '2023-03-03 10:00:00', 'john1'),
+        ('Wireless headphones', 'World-class combination of noise cancelling performance and premium comfort', 'Electronics', '275.00', '2023-03-30 12:00:00', 'jane2'),
+        ('Artisan Stand Mixer', 'Mix ingredients with ease using this powerful and stylish stand mixer', 'Home & Garden', '250.00', '2023-03-17 13:15:00', 'jane2'),
+        ('Mountain Bike', 'Full suspension mountain bike with 29-inch wheels', 'Sporting Goods', '1000.00', '2023-03-28 16:00:00', 'matt3'),
+        ('Vintage Watch', 'Mechanical wristwatch from the 1950s', 'Clothing & Accessories', '300.00', '2023-03-27 14:20:00', 'matt3'),
+        ('Leather Shoulder Bag', 'Womens crossbody bag with adjustable strap', 'Clothing & Accessories', '400.00', '2023-03-13 10:30:00', 'lisa4'),
+        ('Coffee Maker', 'Programmable coffee machine with thermal carafe', 'Home & Garden', '50.00', '2023-03-07 08:15:00', 'lisa4')",
 
     "INSERT INTO review(remark, score, reviewDate, writtenBy, forItem) VALUES
-    ('Great phone. The battery lasts all day!', 'excellent', '2023-04-03 10:02:00', 'jane2', '1'),
-    ('These headphones are fantastic! The noise cancelling is top notch!', 'excellent', '2023-04-02 18:34:00', 'john1', '2'),
-    ('Amazing sound, however, not comfortable to wear for long periods of time.', 'good', '2023-04-04 16:00:00', 'alice5', '2'),
-    ('I love my new mixer! I wish the bowl was larger.', 'good', '2023-04-01 11:45:00', 'lisa4', '3'),
-    ('Too loud and too heavy. Cookies turned out great though.', 'fair', '2023-04-01 20:25:00', 'matt3', '3'),
-    ('The suspension is terrible! Would not recommend it.', 'poor', '2023-04-02 18:58:00', 'john1', '4'),
-    ('Very cute, I got so many compliments.', 'excellent', '2023-04-03 08:24:00', 'jane2', '6'),
-    ('Broke after 2 months and difficult to clean. Avoid!', 'poor', '2023-04-04 16:30:00', 'alice5', '7')"
+        ('Great phone. The battery lasts all day!', 'excellent', '2023-04-03 10:02:00', 'jane2', '1'),
+        ('These headphones are fantastic! The noise cancelling is top notch!', 'excellent', '2023-04-02 18:34:00', 'john1', '2'),
+        ('Amazing sound, however, not comfortable to wear for long periods of time.', 'good', '2023-04-04 16:00:00', 'alice5', '2'),
+        ('I love my new mixer! I wish the bowl was larger.', 'good', '2023-04-01 11:45:00', 'lisa4', '3'),
+        ('Too loud and too heavy. Cookies turned out great though.', 'fair', '2023-04-01 20:25:00', 'matt3', '3'),
+        ('The suspension is terrible! Would not recommend it.', 'poor', '2023-04-02 18:58:00', 'john1', '4'),
+        ('Very cute, I got so many compliments.', 'excellent', '2023-04-03 08:24:00', 'jane2', '6'),
+        ('Broke after 2 months and difficult to clean. Avoid!', 'poor', '2023-04-04 16:30:00', 'alice5', '7')"
 
 );
 
